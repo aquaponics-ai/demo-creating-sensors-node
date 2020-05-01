1067327ddb67b3b82c146a1e0e71ba77f5c771e7
Jonathan Reyes <me@jonathanreyes.com>
2020-05-01 10:32:35 +0300

    Updated comments and readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Aquaponics AI Sensor Setup (Node)
+
+An example script you can use to setup your sensor using Node JS. 
+You can use node on most DIY boards and things like Raspberry Pi.
+
+### Getting Started
+
+```sh
+git clone https://github.com/aquaponics-ai/demo-creating-sensors-node.git
+cd demo-creating-sensors-node
+npm install
+node sensor.js
+```
--- sensor.js
+++ sensor.js
@@ -1,5 +1,10 @@
 const awsIot = require('aws-iot-device-sdk')
 
+/**
+ * The files referenced here are downloaded through the dashboard
+ * when generating your sensor.  You will also find the host endpoint
+ * in your dashboard as well.
+ */
 const client = awsIot.device({
   keyPath: './certs/XXXXXXXXXXX-private.key',
   certPath: './certs/XXXXXXXXXXX-cert.pem',
@@ -8,13 +13,33 @@ const client = awsIot.device({
   host: 'XXXXXXXXXX-ats.iot.us-east-1.amazonaws.com'
 })
 
+/**
+ * This is not required but shows you how you can add various listeners to the 
+ * client and observe the different events.
+ */
 client.on('connect', () => console.log('Successfully connected to Aquaponics AI'))
 
-// You can retrieve this topic from your dashboard under the selected sensor
-// This topic relates to a specific notebook. You can record metrics to separate
-// notebooks to keep everything organized.
-const topic = 'logs/at-i/5a3a1cae-8715-41a7-9ffe-9f2d04ba65d0/cd3d9bb0-a4a7-4236-92aa-257f871dc72a'
+/**
+ * You can retrieve this topic from your dashboard under the selected sensor
+ * This topic relates to a specific notebook. You can record metrics to separate
+ * notebooks to keep everything organized.
+ */
+const topic = 'logs/at-i/5a3a1cae-8715-41a7-9ffe-9f2d04ba65d0/ad3d9bb0-a4a7-4246-92aa-257f871dc72a'
 
+/**
+ * This function is the only function you need to send data to Aquaponics AI.
+ * By utilizing the AWS IoT Device SDK you can also tap into some fantastic
+ * handling of intermittent connectivity, shadows, and more. You can check out
+ * the full documentation on the AWS website.
+ * 
+ * When sending data to the IoT endpoint please include the following attributes
+ * in your object:
+ * 
+ * {
+ *   "dt": String,  # DateTime object in an ISO format.
+ *   "v": Float     # Float value of the sensor reading.
+ * }
+ */
 client.publish(topic, JSON.stringify({
     // The current date time by your actual location
     dt: new Date().toISOString(), 
